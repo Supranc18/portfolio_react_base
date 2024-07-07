e9cee28c6d325c9ed98227149095b3a263e95a20
supranc18 <supranc.shrestha@gmail.com>
2024-07-07 13:15:15 +0545

    navbar hamburger

--- src/Components/navbar/Nav.jsx
+++ src/Components/navbar/Nav.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./nav.css"
 import {Link} from 'react-scroll'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";  
 
 
 export default function Nav() {
-
+  const[sidenav,setSidenav]=useState('closenav')
+  function hamburger() {
+    setSidenav('sidenav') 
+  }
+  function close() {
+    setSidenav('closenav') 
+  }
 
 
   return (
@@ -14,12 +22,25 @@ export default function Nav() {
       <img src="/Logo.png" alt="" />
       <p>Portfolio</p>
     </div>
+    <div className={sidenav}>
+    <ul>
+      <div onClick={close} className="close">
+    < IoClose />
+    <hr />
+    </div>
+        <li><Link to="home" smooth={true} offset={200} duration={600}>Home</Link></li>
+        <li><Link to="project" smooth={true} offset={200} duration={600}>Projects</Link></li>
+        <li><Link to="resume" smooth={true} offset={200} duration={600}>Resume</Link></li>
+        <li><Link to="about" smooth={true} offset={200} duration={600}>About</Link></li>
+      </ul>
+    </div>
     <div className="rightnav">
       <ul>
         <li><Link to="home" smooth={true} offset={200} duration={600}>Home</Link></li>
         <li><Link to="project" smooth={true} offset={200} duration={600}>Projects</Link></li>
         <li><Link to="resume" smooth={true} offset={200} duration={600}>Resume</Link></li>
         <li><Link to="about" smooth={true} offset={200} duration={600}>About</Link></li>
+        <li onClick={hamburger} className='burger'><GiHamburgerMenu /></li>
       </ul>
     </div>
    </div>
--- src/Components/projects/Projects.jsx
+++ src/Components/projects/Projects.jsx
@@ -9,41 +9,45 @@ export default function Projects() {
     <div className="mainproject" id='project' >
         <h2>My Projects</h2>
         <div className="containar">
-       <a href="https://weather-app-with-api-blond.vercel.app/"><div className="box">
-            <div className="imgcontain">
+       <div className="box">
+       <a href="https://weather-app-with-api-blond.vercel.app/"> <div className="imgcontain">
             <img src="/weather.png" alt="" />
             </div>
+            </a>
             <div className="info">
             <h3>Weather App</h3>
             <a href="https://github.com/Supranc18/portfolio_react_base"><FaGithub/></a>
             </div>
             <p>This is a react project with api.This app display the current weather conditions for almost every location.</p>
         </div>
-        </a> 
+       
 
-        <a href="https://microsoft-home-demo.vercel.app/"><div className="box">
-            <div className="imgcontain">
+        <div className="box">
+        <a href="https://microsoft-home-demo.vercel.app/"><div className="imgcontain">
             <img src="/Microsoft demo.png" alt="" />
             </div>
+            </a> 
             <div className="info">
             <h3>Microsoft Clone</h3>
             <a href="https://github.com/Supranc18/Microsoft-home-demo"><FaGithub/></a>
             </div>
             <p>This is a HTML and CSS project.This is a clone page of microsoft</p>
         </div>
-        </a> 
+        
 
-        <a href="https://project-1-pi-nine.vercel.app/"><div className="box">
+       <div className="box">
+       <a href="https://project-1-pi-nine.vercel.app/">
             <div className="imgcontain">
             <img src="/Ecommers.png" alt="" />
             </div>
+            </a> 
             <div className="info">
             <h3>Ecommerce clone</h3>
             <a href="https://github.com/Supranc18/Project-1"><FaGithub/></a>
             </div>
             <p>This is a HTM,CSS and Javascript project. This is a ecommerce clone website of champion brand</p>
         </div>
-        </a> 
+        
     
         
         </div>  
